a5d8816c626332b3370fe617b2f928e1bc248d3e
iampr <iampr@PRABIN>
2023-01-04 14:40:05 +0545

    Add project files.

---
+++ Asabeginner8/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class PPPP
+{
+    static void Main()
+    {
+        int[] evennumbers = { 2, 4, 6, 8, 10 };
+        int[] oddnumbers = { 1, 3, 5, 7, 9 };
+        int sum = evennumbers[0] + oddnumbers[2];
+        Console.WriteLine(sum);
+
+    }
+}
